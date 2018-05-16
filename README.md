b811135ba053de45bd9c847a8d76639440af4bb9
Stefano Kaefer <stefanokaefer@gmail.com>
2018-05-16 01:16:26 -0300

    dotenv

--- README.md
+++
@@ -1,2 +0,0 @@
-# onlineNode
-Heroku up
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const envPath = process.env.NODE_ENV
-  ? `.env.${process.env.NODE_ENV}`
-  : '..env';
-
-require('dotenv').config({ path: envPath });
+require('dotenv').config();
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
-const Raven = require('./app/services/sentry');
+
 
 const dbConfig = require('./config/database');
 
@@ -16,11 +12,7 @@ requireDir(dbConfig.modelsPath);
 
 app.use(bodyParser.json());
 
-app.use(Raven.requestHandler());
-
 app.use('/api', require('./app/routes'));
-
-app.use(Raven.errorHandler());
 
 app.listen(3000);
 
