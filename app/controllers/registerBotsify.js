1e4a77efe6c2e4eeff7c8ae0c7d62a3bbb1cdc4c
StefanoMat <stefanokaefer@gmail.com>
2018-05-17 01:57:20 -0300

    Update registerBotsify.js

--- app/controllers/registerBotsify.js
+++ app/controllers/registerBotsify.js
@@ -5,7 +5,7 @@ const Register = mongoose.model('Register');
 module.exports = {
   async create(req, res, next) {
     try {
-      const register = await Register.create({ content: req.body.dia });
+      const register = await Register.create({ content: ...req.body });
       return res.json(register);
     } catch (err) {
       return next(err);
